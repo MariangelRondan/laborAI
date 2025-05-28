b9cffb183f5fed7d80bca45c45f7d2f644eadab5
Mariangel <rondanmariangel@gmail.com>
2025-05-28 14:51:24 -0300

    volviendo al simulated response

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,36 +1,31 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
-  api_url = 'http://localhost:8000/get_answer'
+  api_url = 'ksdfksdjbsjksdn';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
-
-  getChatResponse(question: string) {
-    const body = { question: question }; 
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-    return this.http.post<any>(this.api_url, body, { headers: headers });
+  getChatResponse(question: string): Observable<string> {
+    // Simular una respuesta del backend
+    const simulatedResponses: any = {
+      '¿Cuál es la duración máxima de la jornada laboral?':
+        'La duración máxima es de 8 horas diarias y 48 horas semanales.',
+      '¿Qué deben pagarme en la liquidación cuando me despiden?':
+        'En la liquidación se deben incluir salarios pendientes, vacaciones no gozadas, indemnización por despido, entre otros conceptos.',
+    };
+    const response: any =
+      simulatedResponses[question]! ||
+      'Lo siento, no tengo una respuesta para esa pregunta en este momento.';
+    return of(response);
   }
 
-  saveHistory(hisotry: any){
-    const historyString = JSON.stringify(hisotry)
-    localStorage.setItem('historialLabor', historyString)
+  saveHistory(hisotry: any) {
+    const historyString = JSON.stringify(hisotry);
+    localStorage.setItem('historialLabor', historyString);
   }
 }
-  
-  // Simular una respuesta del backend
-  // const simulatedResponses: any = {
-  //   "¿Cuál es la duración máxima de la jornada laboral?": "La duración máxima es de 8 horas diarias y 48 horas semanales.",
-  //   "¿Qué deben pagarme en la liquidación cuando me despiden?": "En la liquidación se deben incluir salarios pendientes, vacaciones no gozadas, indemnización por despido, entre otros conceptos."
-  // };
-  // const response: any = simulatedResponses[question]! || 'Lo siento, no tengo una respuesta para esa pregunta en este momento.';
-  // return of(response);
-
-
